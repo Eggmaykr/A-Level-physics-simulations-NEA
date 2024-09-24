6d11e7ade1b1471a71cbd42f080efc99a1b58297
Eggmaykr <eggmaykr@gmail.com>
2024-09-24 11:45:54 +0100

    QuickFix

--- Scripts/TimerScript.gd
+++ Scripts/TimerScript.gd
@@ -4,7 +4,7 @@ var Repeating : bool = false
 
 
 func _process(delta):
-	get_node("VBoxContainer/TimeLeft").text = str(get_node("Timer").wait_time) + "/" + str(get_node("Timer").time_left)
+	get_node("VBoxContainer/TimeLeft").text = str(get_node("Timer").wait_time) + "/" + str(int(get_node("Timer").time_left))
 
 func _on_Timer_timeout():
 	get_parent().get_parent().get_node("Sockets").get_child(0)._activate_Sender(false)
@@ -17,11 +17,15 @@ func _on_TouchScreenButton_pressed():
 
 
 func _on_Start_pressed():
-	get_node("Timer").start(get_node("Timer").wait_time)
+	if get_node("Timer").is_stopped() == false:
+		get_node("Timer").stop()
+	else:
+		get_node("Timer").start(get_node("Timer").wait_time)
 
 
 func _on_CheckButton_gui_input(event):
 	if event is InputEventScreenTouch:
+		print("Touched")
 		if event.pressed == true:
 			if Repeating == false:
 				Repeating = true
@@ -34,4 +38,4 @@ func _on_CheckButton_gui_input(event):
 func _on_HSlider_value_changed(value):
 	get_node("Timer").stop()
 	get_node("Timer").wait_time = value
-	get_node("VBoxContainer/TimeLeft").text = str(value) + "/" + str(get_node("Timer").time_left)
+	get_node("VBoxContainer/TimeLeft").text = str(value) + "/" + str(int(get_node("Timer").time_left))
