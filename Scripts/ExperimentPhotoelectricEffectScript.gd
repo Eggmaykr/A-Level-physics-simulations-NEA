dda8ca03508fdb58c33e7ec962687f4fc29c91f6
Eggmaykr <eggmaykr@gmail.com>
2024-09-30 12:00:22 +0100

    Photoelectric Effect Changes color

---
+++ Scripts/ExperimentPhotoelectricEffectScript.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var TimeElapsed : float = 0.0
+var FrequencyHertz : float = 0.0
+var WavelengthMetersXTenMillion : float = 100.0
+
+
+func _ready():
+	for i in range(100):
+		#y = sin(deg_to_rad(i))
+		var Point = Vector2(i*1.4, sin(i*0.4)*20.0)
+		get_node("LightWave").add_point(Point)
+	
+
+func _process(delta):
+	TimeElapsed += delta+0.2
+	for i in range(100):
+		get_node("LightWave").points[i].y = sin((i+TimeElapsed)/WavelengthMetersXTenMillion*1000.0)*20.0
+
+
+func _on_Wavelength_value_changed(value):
+	WavelengthMetersXTenMillion = value
+	get_node("LightWave").default_color = Color.from_hsv((0.720-(1.0/8000.0)*WavelengthMetersXTenMillion),1.0, 0.82,1.0)
